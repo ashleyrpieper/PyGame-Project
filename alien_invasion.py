d2e81c431b615198a072f650dd81aa507c221528
Ashley Pieper <ashleypieper@Ashleys-MacBook-Pro.local>
2020-03-03 20:30:00 -0600

    dropping the fleet and changing direction

--- alien_invasion.py
+++ alien_invasion.py
@@ -85,14 +85,16 @@ class AlienInvasion:
         self.bullets.update()
 
         # Get rid of bullets that have disappeared.
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
-            # print(len(self.bullets))      # Created to verify bullets are being deleted
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+        # print(len(self.bullets))      # Created to verify bullets are being deleted
     
     def _update_aliens(self):
-        """Update the positions of all aliens in the fleet."""
-        self.aleins.update()
+        """Check if the fleet is at an edge,
+            then update the positions of all aliens in the fleet."""
+        self._check_fleet_edges()
+        self.aliens.update()
     
     def _create_fleet(self):
         """Create the fleet of aliens."""
@@ -121,6 +123,19 @@ class AlienInvasion:
         alien.rect.x = alien.x
         alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.alien.add(alien)
+    
+    def _check_fleet_edges(self):
+        """Respond appropriately if any aliens have reached an edge."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+    
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet's direction."""
+        for alien in self.aliens.sprite():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
