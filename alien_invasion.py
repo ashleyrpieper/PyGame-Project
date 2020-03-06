d012cf7d48c0cec64e7a7dd0b38e50c34527b819
Ashley Pieper <ashleypieper@Ashleys-MacBook-Pro.local>
2020-03-05 18:29:11 -0600

    detecting alien and ship collisions

--- alien_invasion.py
+++ alien_invasion.py
@@ -107,6 +107,10 @@ class AlienInvasion:
             then update the positions of all aliens in the fleet."""
         self._check_fleet_edges()
         self.aliens.update()
+
+        # Look for alien-ship collisions.
+        if pygame.sprite.spritecollideany(self.ship, self.aliens):
+            print("Ship hit!!!")
     
     def _create_fleet(self):
         """Create the fleet of aliens."""
